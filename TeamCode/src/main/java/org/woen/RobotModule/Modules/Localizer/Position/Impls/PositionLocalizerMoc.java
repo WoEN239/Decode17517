949178cb1876f056e6795e08c7506aeec5c0951e
EgorKhvostikov <egorhvog@gmail.com>
2025-09-19 16:04:18 +0300

    feat(Localizer moc) add localizer MOCs

--- TeamCode/src/main/java/org/woen/RobotModule/Modules/Localizer/Position/Impls/PositionLocalizerMoc.java
+++ TeamCode/src/main/java/org/woen/RobotModule/Modules/Localizer/Position/Impls/PositionLocalizerMoc.java
@@ -1,6 +1,30 @@
 package org.woen.RobotModule.Modules.Localizer.Position.Impls;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+
+import org.woen.Config.MatchData;
+import org.woen.RobotModule.Modules.Localizer.Position.Architecture.LocalPositionObserver;
+import org.woen.RobotModule.Modules.Localizer.Position.Architecture.PositionObserver;
 import org.woen.RobotModule.Modules.Localizer.Position.Interface.PositionLocalizer;
+import org.woen.Telemetry.ConfigurableVariables.Provider;
+import org.woen.Util.Vectors.Pose;
 
 public class PositionLocalizerMoc implements PositionLocalizer {
+    private final PositionObserver positionObserver = new PositionObserver();
+    private final LocalPositionObserver localPositionObserver = new LocalPositionObserver();
+
+    private final Provider<Pose> positionProvider = new Provider<>(MatchData.startPosition);
+    private final Provider<Pose> localPositionProvider = new Provider<>(new Pose(0,0,0));
+
+    @Override
+    public void update() {
+       positionObserver.notifyListeners(positionProvider.get());
+       localPositionObserver.notifyListeners(localPositionProvider.get());
+    }
+
+    @Override
+    public void init() {
+        FtcDashboard.getInstance().addConfigVariable("manual_localizer","pos",positionProvider);
+        FtcDashboard.getInstance().addConfigVariable("manual_localizer","local_pos",localPositionProvider);
+    }
 }
--- TeamCode/src/main/java/org/woen/RobotModule/Modules/Localizer/Velocity/Impls/VelocityLocalizerMoc.java
+++ TeamCode/src/main/java/org/woen/RobotModule/Modules/Localizer/Velocity/Impls/VelocityLocalizerMoc.java
@@ -1,6 +1,29 @@
 package org.woen.RobotModule.Modules.Localizer.Velocity.Impls;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+
+import org.woen.RobotModule.Modules.Localizer.Velocity.Architecture.LocalVelocityObserver;
+import org.woen.RobotModule.Modules.Localizer.Velocity.Architecture.VelocityObserver;
 import org.woen.RobotModule.Modules.Localizer.Velocity.Interface.VelocityLocalizer;
+import org.woen.Telemetry.ConfigurableVariables.Provider;
+import org.woen.Util.Vectors.Pose;
 
 public class VelocityLocalizerMoc implements VelocityLocalizer {
+    private final VelocityObserver velocityObserver = new VelocityObserver();
+    private final LocalVelocityObserver localVelocityObserver = new LocalVelocityObserver();
+
+    private final Provider<Pose> velocityProvider = new Provider<>(new Pose(0,0,0));
+    private final Provider<Pose> localVelocityProvider = new Provider<>(new Pose(0,0,0));
+
+    @Override
+    public void update() {
+        velocityObserver.notifyListeners(velocityProvider.get());
+        localVelocityObserver.notifyListeners(localVelocityProvider.get());
+    }
+
+    @Override
+    public void init() {
+        FtcDashboard.getInstance().addConfigVariable("manual_localizer","vel",velocityProvider);
+        FtcDashboard.getInstance().addConfigVariable("manual_localizer","local_vel", localVelocityProvider);
+    }
 }
