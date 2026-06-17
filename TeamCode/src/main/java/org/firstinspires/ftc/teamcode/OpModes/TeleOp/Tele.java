9de9e35a3caaa8cf1950ae02dc1248ee1ca02598
Max <mitrusinmaksim@gmail.com>
2026-06-17 23:29:15 +0300

    fix() tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/Tele.java
@@ -1,13 +1,11 @@
 package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
 
 import static com.pedropathing.math.MathFunctions.normalizeAngleSigned;
-
 import static java.lang.Math.PI;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.bylazar.configurables.annotations.Configurable;
-
 import com.bylazar.field.FieldManager;
 import com.bylazar.field.PanelsField;
 import com.bylazar.field.Style;
@@ -21,6 +19,7 @@ import com.pedropathing.math.Vector;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Modules.FSM;
 import org.firstinspires.ftc.teamcode.Modules.FSM_STATE;
@@ -31,49 +30,14 @@ import org.firstinspires.ftc.teamcode.Pedro.Constants;
 @TeleOp
 public class Tele extends OpMode {
     private Follower follower;
-    public static Pose startingPose; //See ExampleAuto to understand how to use this
     private TelemetryManager telemetryM;
 
-    public static final double ROBOT_RADIUS = 9; // woah
+    public static final double ROBOT_RADIUS = 9;
     private static final FieldManager panelsField = PanelsField.INSTANCE.getField();
 
-    private static final Style robotLook = new Style(
-            "", "#3F51B5", 0.75
-    );
+    private static final Style robotLook = new Style("", "#3F51B5", 0.75);
 
-    @Override
-    public void init() {
-        follower = Constants.createFollower(hardwareMap);
-        follower.setStartingPose(new Pose(0, 0, PI));
-        follower.update();
-        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
-    }
-
-    //Turret turret = new Turret();
-
-    public static double angle = 30;
     FSM fsm = new FSM();
-
-    public static double k = 1;
-
-    @Override
-    public void start() {
-        //turret.start(hardwareMap,()->follower.getPose().getHeading());
-
-        fsm.start(hardwareMap, follower);
-        enc = hardwareMap.get(
-                DcMotorEx.class, "motor_lb"
-        );
-
-    }
-
-    public static PIDFCoefficients xPidC = new PIDFCoefficients(0.015, 0, 0, 0.012);
-    public static PIDFCoefficients yPidC = new PIDFCoefficients(0.02, 0, 0, 0.0154);
-    public static PIDFCoefficients hPidC = new PIDFCoefficients(0.05, 0, 0, 0.14);
-    private PIDFController xPid = new PIDFController(xPidC);
-    private PIDFController yPid = new PIDFController(yPidC);
-    private PIDFController hPid = new PIDFController(hPidC);
-
 
     boolean isAngleControl = true;
     double angleToControl = PI * 0.5;
@@ -82,121 +46,89 @@ public class Tele extends OpMode {
 
     boolean old_left_bumper = false;
     int count_press = 0;
-
     boolean isItReversed = false;
 
-    DcMotorEx enc;
+    ElapsedTime telemetryTimer = new ElapsedTime();
 
+    @Override
+    public void init() {
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(new Pose(0, 0, 0));
+
+        follower.drivetrain.breakFollowing() ;
+
+        follower.update();
+        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+    }
+
+    @Override
+    public void start() {
+        fsm.start(hardwareMap, follower);
+        telemetryTimer.reset();
+    }
 
     @Override
     public void loop() {
-        //Call this once per loop
-        updateAll();
-
-
-        xPid.setCoefficients(xPidC);
-        hPid.setCoefficients(hPidC);
-        yPid.setCoefficients(yPidC);
-
-        Vector sticks = new Vector();
-        sticks.setOrthogonalComponents(fpv(-gamepad1.left_stick_y), fpv(-gamepad1.left_stick_x));
-        sticks.rotateVector(-follower.getPose().getHeading() + PI * 0.5);
-
-        Vector vel = follower.getVelocity();
-        vel.rotateVector(-follower.getPose().getHeading());
-
-
-        double x = sticks.getXComponent();
-
-
-        double y = sticks.getYComponent();
-
-        double h = fpv(-gamepad1.right_stick_x);
-
-        telemetryM.debug(hPid.getError());
+        double forward = fpv(-gamepad1.left_stick_y);
+        double strafe = fpv(-gamepad1.left_stick_x);
+        double turn = fpv(-gamepad1.right_stick_x);
 
         isAngleControl = gamepad1.right_trigger > 0.1;
         if (isAngleControl) {
             anglePid.setCoefficients(anglePidC);
             anglePid.updateError(normalizeAngleSigned(angleToControl - follower.getHeading()));
-            h = anglePid.run();
+            turn = anglePid.run();
         }
 
-        double lf = x - h - y;
-        double rf = x + h + y;
-        double rb = x + h - y;
-        double lb = x - h + y;
+        follower.setTeleOpDrive(forward, strafe, turn, false);
 
-        follower.drivetrain.runDrive(
-                new double[]{lf, lb, rf, rb}
-        );
+        follower.update();
+        fsm.update();
 
-        telemetryM.debug("r x", vel.getXComponent());
-
-        FtcDashboard.getInstance().getTelemetry().addData("x", follower.getPose().getX());
-
-        FtcDashboard.getInstance().getTelemetry().addData("y", follower.getPose().getY());
-
-        FtcDashboard.getInstance().getTelemetry().addData("h", Math.toDegrees(follower.getPose().getHeading()));
-        telemetryM.debug("r y", vel.getYComponent());
-        telemetryM.debug("r h", follower.getAngularVelocity());
-        FtcDashboard.getInstance().getTelemetry().addData("angele ",enc.getCurrentPosition() * k);
-        FtcDashboard.getInstance().getTelemetry().update();
         if (gamepad1.left_trigger > 0.1) {
             fsm.setState(FSM_STATE.REVERSE);
             isItReversed = true;
         }
-        if(gamepad1.left_trigger < 0.1 && isItReversed){
+        if (gamepad1.left_trigger < 0.1 && isItReversed) {
             fsm.setState(FSM_STATE.EAT);
             isItReversed = false;
         }
-        if (gamepad1.left_bumper && !old_left_bumper && count_press == 0) {
-            fsm.setState(FSM_STATE.DRIVE);
-            count_press = 1;
-        } else if (gamepad1.left_bumper && !old_left_bumper && count_press == 1) {
-            fsm.setState(FSM_STATE.SHOOT);
-            count_press = 0;
+
+        if (gamepad1.left_bumper && !old_left_bumper) {
+            if (count_press == 0) {
+                fsm.setState(FSM_STATE.DRIVE);
+                count_press = 1;
+            } else {
+                fsm.setState(FSM_STATE.SHOOT);
+                count_press = 0;
+            }
         }
-
-        if(gamepad1.square)
-            fsm.setState(FSM_STATE.SHOOT_LEFT);
-        if(gamepad1.triangle)
-            fsm.setState(FSM_STATE.SHOOT_CENTER);
-        if(gamepad1.circle)
-            fsm.setState(FSM_STATE.SHOOT_RIGHT);
-
-
-        //turret.debug(telemetryM);
-
-
-
-
-        panelsField.setStyle(robotLook);
-        panelsField.moveCursor(follower.getPose().getX(), follower.getPose().getY());
-        panelsField.circle(ROBOT_RADIUS);
-
-        Vector v = follower.getPose().getHeadingAsUnitVector();
-        v.setMagnitude(v.getMagnitude() * ROBOT_RADIUS);
-        double x1 = follower.getPose().getX() + v.getXComponent() / 2, y1 = follower.getPose().getY() + v.getYComponent() / 2;
-        double x2 = follower.getPose().getX() + v.getXComponent(), y2 = follower.getPose().getY() + v.getYComponent();
-
-        panelsField.setStyle(robotLook);
-        panelsField.moveCursor(x1, y1);
-        panelsField.line(x2, y2);
-        panelsField.update();
         old_left_bumper = gamepad1.left_bumper;
 
-    }
+        if (gamepad1.square) fsm.setState(FSM_STATE.SHOOT_LEFT);
+        if (gamepad1.triangle) fsm.setState(FSM_STATE.SHOOT_CENTER);
+        if (gamepad1.circle) fsm.setState(FSM_STATE.SHOOT_RIGHT);
 
-    private void updateAll() {
-        follower.update();
-        telemetryM.update();
-        // turret.update();
-        fsm.update();
+        if (telemetryTimer.milliseconds() > 100) {
+            panelsField.setStyle(robotLook);
+            panelsField.moveCursor(follower.getPose().getX(), follower.getPose().getY());
+            panelsField.circle(ROBOT_RADIUS);
+
+            Vector v = follower.getPose().getHeadingAsUnitVector();
+            v.setMagnitude(v.getMagnitude() * ROBOT_RADIUS);
+            panelsField.line(follower.getPose().getX() + v.getXComponent(), follower.getPose().getY() + v.getYComponent());
+            panelsField.update();
+
+            telemetryM.debug("X", follower.getPose().getX());
+            telemetryM.debug("Y", follower.getPose().getY());
+            telemetryM.debug("Heading", follower.getPose().getHeading());
+            telemetryM.update();
+
+            telemetryTimer.reset();
+        }
     }
 
     private double fpv(double x) {
         return (1 - 0.68) * x + 0.68 * x * x * x;
     }
 }
-
